c08deabb1642156cfb609fa38a9494d8e71f3089
Turbobot <turbobot@vercel.com>
2024-09-17 17:11:27 +1200

    feat(create-turbo): apply official-starter transform

--- apps/dsu/tests/example.spec.ts
+++ apps/dsu/tests/example.spec.ts
@@ -3,7 +3,8 @@ import config from '../playwright.config';
 
 test('dsu test', async ({ page }) => {
   console.log(config.DSU_URL);
-  await page.goto(config.DSU_URL);
+  await page.goto(config.DSU_URL)
+  
 });
 
 
